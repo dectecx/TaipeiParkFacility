f82ddc50c848938f1f0c002268a3637de8f53dc9
ts6075 <75653667+ts6075@users.noreply.github.com>
2021-02-25 12:06:19 +0800

    feat: 修改起始載入頁面

--- Tpf.Web/App_Start/RouteConfig.cs
+++ Tpf.Web/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Tpf.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Panel", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
