2f5bda066f6c0d65ec3cdd865ca71f6ca493d7ea
ts6075 <75653667+ts6075@users.noreply.github.com>
2021-02-25 13:05:33 +0800

    feat: add TransactionScope

--- Tpf.Library/Services/PanelService.cs
+++ Tpf.Library/Services/PanelService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Transactions;
 using Tpf.Library.ViewModels;
 //using System.Transactions;
 
@@ -32,11 +33,17 @@ namespace Tpf.Library.Services
 
         public void Delete()
         {
-            foreach (var item in db.ParkFacility)
-                db.ParkFacility.Remove(item);
-            foreach (var item in db.Park)
-                db.Park.Remove(item);
-            db.SaveChanges();
+            using (TransactionScope scope = new TransactionScope())
+            {
+                foreach (var item in db.ParkFacility)
+                    db.ParkFacility.Remove(item);
+                foreach (var item in db.Park)
+                    db.Park.Remove(item);
+
+                db.SaveChanges();
+
+                scope.Complete();
+            }
         }
 
         public void Patch(List<OpdGovRawVModel> vmodels)
@@ -46,184 +53,189 @@ namespace Tpf.Library.Services
                 return;
             }
 
-            //using (TransactionScope scope = new TransactionScope())
-            //{
-            //    scope.Complete();
-            //}
-            var dimFacility = db.DimFacility.Where(e => !e.IsDelete).ToList();
-            foreach (var item in db.ParkFacility)
-                db.ParkFacility.Remove(item);
-            foreach (var item in db.Park)
-                db.Park.Remove(item);
-            foreach (var item in vmodels)
+            using (TransactionScope scope = new TransactionScope())
             {
-                var model = new Models.Park
+                var dimFacility = db.DimFacility.Where(e => !e.IsDelete).ToList();
+                foreach (var item in db.ParkFacility)
+                    db.ParkFacility.Remove(item);
+                foreach (var item in db.Park)
+                    db.Park.Remove(item);
+                foreach (var item in vmodels)
                 {
-                    Name = item.ParkName,
-                    DictName = item.Dict,
-                    ManagementUnit = item.ManagementUnit,
-                    IsDelete = false,
-                    CreateTime = DateTime.Now,
-                    CreateBy = "user",
-                    UpdateTime = DateTime.Now,
-                    UpdateBy = "user"
-                };
-                db.Park.Add(model);
+                    #region add Park
+                    var model = new Models.Park
+                    {
+                        Name = item.ParkName,
+                        DictName = item.Dict,
+                        ManagementUnit = item.ManagementUnit,
+                        IsDelete = false,
+                        CreateTime = DateTime.Now,
+                        CreateBy = "user",
+                        UpdateTime = DateTime.Now,
+                        UpdateBy = "user"
+                    };
+                    db.Park.Add(model);
+                    #endregion
 
-                if (!string.IsNullOrEmpty(item.CombinedPlayEquipment) && item.CombinedPlayEquipment?.Trim() != "0")
-                {
-                    var facilityId = dimFacility.Where(e => e.Name == "組合遊具").Select(e => e.Id).First();
-                    int.TryParse(item.CombinedPlayEquipment?.Trim(), out int cnt);
-                    var fmodel = new Models.ParkFacility
+                    #region add ParkFacility
+                    if (!string.IsNullOrEmpty(item.CombinedPlayEquipment) && item.CombinedPlayEquipment?.Trim() != "0")
                     {
-                        ParkId = model.Id,
-                        FacilityId = facilityId,
-                        Cnt = cnt,
-                        IsDelete = false,
-                        CreateTime = DateTime.Now,
-                        CreateBy = "user",
-                        UpdateTime = DateTime.Now,
-                        UpdateBy = "user"
-                    };
-                    db.ParkFacility.Add(fmodel);
-                }
-                if (!string.IsNullOrEmpty(item.MillstoneSlide) && item.MillstoneSlide?.Trim() != "0")
-                {
-                    var facilityId = dimFacility.Where(e => e.Name == "磨石滑梯").Select(e => e.Id).First();
-                    int.TryParse(item.MillstoneSlide?.Trim(), out int cnt);
-                    var fmodel = new Models.ParkFacility
+                        var facilityId = dimFacility.Where(e => e.Name == "組合遊具").Select(e => e.Id).First();
+                        int.TryParse(item.CombinedPlayEquipment?.Trim(), out int cnt);
+                        var fmodel = new Models.ParkFacility
+                        {
+                            ParkId = model.Id,
+                            FacilityId = facilityId,
+                            Cnt = cnt,
+                            IsDelete = false,
+                            CreateTime = DateTime.Now,
+                            CreateBy = "user",
+                            UpdateTime = DateTime.Now,
+                            UpdateBy = "user"
+                        };
+                        db.ParkFacility.Add(fmodel);
+                    }
+                    if (!string.IsNullOrEmpty(item.MillstoneSlide) && item.MillstoneSlide?.Trim() != "0")
                     {
-                        ParkId = model.Id,
-                        FacilityId = facilityId,
-                        Cnt = cnt,
-                        IsDelete = false,
-                        CreateTime = DateTime.Now,
-                        CreateBy = "user",
-                        UpdateTime = DateTime.Now,
-                        UpdateBy = "user"
-                    };
-                    db.ParkFacility.Add(fmodel);
-                }
-                if (!string.IsNullOrEmpty(item.Swing) && item.Swing?.Trim() != "0")
-                {
-                    var facilityId = dimFacility.Where(e => e.Name == "鞦韆").Select(e => e.Id).First();
-                    int.TryParse(item.Swing?.Trim(), out int cnt);
-                    var fmodel = new Models.ParkFacility
+                        var facilityId = dimFacility.Where(e => e.Name == "磨石滑梯").Select(e => e.Id).First();
+                        int.TryParse(item.MillstoneSlide?.Trim(), out int cnt);
+                        var fmodel = new Models.ParkFacility
+                        {
+                            ParkId = model.Id,
+                            FacilityId = facilityId,
+                            Cnt = cnt,
+                            IsDelete = false,
+                            CreateTime = DateTime.Now,
+                            CreateBy = "user",
+                            UpdateTime = DateTime.Now,
+                            UpdateBy = "user"
+                        };
+                        db.ParkFacility.Add(fmodel);
+                    }
+                    if (!string.IsNullOrEmpty(item.Swing) && item.Swing?.Trim() != "0")
                     {
-                        ParkId = model.Id,
-                        FacilityId = facilityId,
-                        Cnt = cnt,
-                        IsDelete = false,
-                        CreateTime = DateTime.Now,
-                        CreateBy = "user",
-                        UpdateTime = DateTime.Now,
-                        UpdateBy = "user"
-                    };
-                    db.ParkFacility.Add(fmodel);
-                }
-                if (!string.IsNullOrEmpty(item.Seesaw) && item.Seesaw?.Trim() != "0")
-                {
-                    var facilityId = dimFacility.Where(e => e.Name == "翹翹板").Select(e => e.Id).First();
-                    int.TryParse(item.Seesaw?.Trim(), out int cnt);
-                    var fmodel = new Models.ParkFacility
+                        var facilityId = dimFacility.Where(e => e.Name == "鞦韆").Select(e => e.Id).First();
+                        int.TryParse(item.Swing?.Trim(), out int cnt);
+                        var fmodel = new Models.ParkFacility
+                        {
+                            ParkId = model.Id,
+                            FacilityId = facilityId,
+                            Cnt = cnt,
+                            IsDelete = false,
+                            CreateTime = DateTime.Now,
+                            CreateBy = "user",
+                            UpdateTime = DateTime.Now,
+                            UpdateBy = "user"
+                        };
+                        db.ParkFacility.Add(fmodel);
+                    }
+                    if (!string.IsNullOrEmpty(item.Seesaw) && item.Seesaw?.Trim() != "0")
                     {
-                        ParkId = model.Id,
-                        FacilityId = facilityId,
-                        Cnt = cnt,
-                        IsDelete = false,
-                        CreateTime = DateTime.Now,
-                        CreateBy = "user",
-                        UpdateTime = DateTime.Now,
-                        UpdateBy = "user"
-                    };
-                    db.ParkFacility.Add(fmodel);
-                }
-                if (!string.IsNullOrEmpty(item.Shake) && item.Shake?.Trim() != "0")
-                {
-                    var facilityId = dimFacility.Where(e => e.Name == "搖搖樂").Select(e => e.Id).First();
-                    int.TryParse(item.Shake?.Trim(), out int cnt);
-                    var fmodel = new Models.ParkFacility
+                        var facilityId = dimFacility.Where(e => e.Name == "翹翹板").Select(e => e.Id).First();
+                        int.TryParse(item.Seesaw?.Trim(), out int cnt);
+                        var fmodel = new Models.ParkFacility
+                        {
+                            ParkId = model.Id,
+                            FacilityId = facilityId,
+                            Cnt = cnt,
+                            IsDelete = false,
+                            CreateTime = DateTime.Now,
+                            CreateBy = "user",
+                            UpdateTime = DateTime.Now,
+                            UpdateBy = "user"
+                        };
+                        db.ParkFacility.Add(fmodel);
+                    }
+                    if (!string.IsNullOrEmpty(item.Shake) && item.Shake?.Trim() != "0")
                     {
-                        ParkId = model.Id,
-                        FacilityId = facilityId,
-                        Cnt = cnt,
-                        IsDelete = false,
-                        CreateTime = DateTime.Now,
-                        CreateBy = "user",
-                        UpdateTime = DateTime.Now,
-                        UpdateBy = "user"
-                    };
-                    db.ParkFacility.Add(fmodel);
-                }
-                if (!string.IsNullOrEmpty(item.Climbing) && item.Climbing?.Trim() != "0")
-                {
-                    var facilityId = dimFacility.Where(e => e.Name == "攀爬組").Select(e => e.Id).First();
-                    int.TryParse(item.Climbing?.Trim(), out int cnt);
-                    var fmodel = new Models.ParkFacility
+                        var facilityId = dimFacility.Where(e => e.Name == "搖搖樂").Select(e => e.Id).First();
+                        int.TryParse(item.Shake?.Trim(), out int cnt);
+                        var fmodel = new Models.ParkFacility
+                        {
+                            ParkId = model.Id,
+                            FacilityId = facilityId,
+                            Cnt = cnt,
+                            IsDelete = false,
+                            CreateTime = DateTime.Now,
+                            CreateBy = "user",
+                            UpdateTime = DateTime.Now,
+                            UpdateBy = "user"
+                        };
+                        db.ParkFacility.Add(fmodel);
+                    }
+                    if (!string.IsNullOrEmpty(item.Climbing) && item.Climbing?.Trim() != "0")
                     {
-                        ParkId = model.Id,
-                        FacilityId = facilityId,
-                        Cnt = cnt,
-                        IsDelete = false,
-                        CreateTime = DateTime.Now,
-                        CreateBy = "user",
-                        UpdateTime = DateTime.Now,
-                        UpdateBy = "user"
-                    };
-                    db.ParkFacility.Add(fmodel);
-                }
-                if (!string.IsNullOrEmpty(item.Sandpit) && item.Sandpit?.Trim() != "0")
-                {
-                    var facilityId = dimFacility.Where(e => e.Name == "戲沙坑").Select(e => e.Id).First();
-                    int.TryParse(item.Sandpit?.Trim(), out int cnt);
-                    var fmodel = new Models.ParkFacility
+                        var facilityId = dimFacility.Where(e => e.Name == "攀爬組").Select(e => e.Id).First();
+                        int.TryParse(item.Climbing?.Trim(), out int cnt);
+                        var fmodel = new Models.ParkFacility
+                        {
+                            ParkId = model.Id,
+                            FacilityId = facilityId,
+                            Cnt = cnt,
+                            IsDelete = false,
+                            CreateTime = DateTime.Now,
+                            CreateBy = "user",
+                            UpdateTime = DateTime.Now,
+                            UpdateBy = "user"
+                        };
+                        db.ParkFacility.Add(fmodel);
+                    }
+                    if (!string.IsNullOrEmpty(item.Sandpit) && item.Sandpit?.Trim() != "0")
                     {
-                        ParkId = model.Id,
-                        FacilityId = facilityId,
-                        Cnt = cnt,
-                        IsDelete = false,
-                        CreateTime = DateTime.Now,
-                        CreateBy = "user",
-                        UpdateTime = DateTime.Now,
-                        UpdateBy = "user"
-                    };
-                    db.ParkFacility.Add(fmodel);
-                }
-                if (!string.IsNullOrEmpty(item.SwingBoat) && item.SwingBoat?.Trim() != "0")
-                {
-                    var facilityId = dimFacility.Where(e => e.Name == "擺盪船索").Select(e => e.Id).First();
-                    int.TryParse(item.SwingBoat?.Trim(), out int cnt);
-                    var fmodel = new Models.ParkFacility
+                        var facilityId = dimFacility.Where(e => e.Name == "戲沙坑").Select(e => e.Id).First();
+                        int.TryParse(item.Sandpit?.Trim(), out int cnt);
+                        var fmodel = new Models.ParkFacility
+                        {
+                            ParkId = model.Id,
+                            FacilityId = facilityId,
+                            Cnt = cnt,
+                            IsDelete = false,
+                            CreateTime = DateTime.Now,
+                            CreateBy = "user",
+                            UpdateTime = DateTime.Now,
+                            UpdateBy = "user"
+                        };
+                        db.ParkFacility.Add(fmodel);
+                    }
+                    if (!string.IsNullOrEmpty(item.SwingBoat) && item.SwingBoat?.Trim() != "0")
                     {
-                        ParkId = model.Id,
-                        FacilityId = facilityId,
-                        Cnt = cnt,
-                        IsDelete = false,
-                        CreateTime = DateTime.Now,
-                        CreateBy = "user",
-                        UpdateTime = DateTime.Now,
-                        UpdateBy = "user"
-                    };
-                    db.ParkFacility.Add(fmodel);
-                }
-                if (!string.IsNullOrEmpty(item.Other) && item.Other?.Trim() != "0")
-                {
-                    var facilityId = dimFacility.Where(e => e.Name == "其他").Select(e => e.Id).First();
-                    int.TryParse(item.Other?.Trim(), out int cnt);
-                    var fmodel = new Models.ParkFacility
+                        var facilityId = dimFacility.Where(e => e.Name == "擺盪船索").Select(e => e.Id).First();
+                        int.TryParse(item.SwingBoat?.Trim(), out int cnt);
+                        var fmodel = new Models.ParkFacility
+                        {
+                            ParkId = model.Id,
+                            FacilityId = facilityId,
+                            Cnt = cnt,
+                            IsDelete = false,
+                            CreateTime = DateTime.Now,
+                            CreateBy = "user",
+                            UpdateTime = DateTime.Now,
+                            UpdateBy = "user"
+                        };
+                        db.ParkFacility.Add(fmodel);
+                    }
+                    if (!string.IsNullOrEmpty(item.Other) && item.Other?.Trim() != "0")
                     {
-                        ParkId = model.Id,
-                        FacilityId = facilityId,
-                        Cnt = cnt,
-                        IsDelete = false,
-                        CreateTime = DateTime.Now,
-                        CreateBy = "user",
-                        UpdateTime = DateTime.Now,
-                        UpdateBy = "user"
-                    };
-                    db.ParkFacility.Add(fmodel);
+                        var facilityId = dimFacility.Where(e => e.Name == "其他").Select(e => e.Id).First();
+                        int.TryParse(item.Other?.Trim(), out int cnt);
+                        var fmodel = new Models.ParkFacility
+                        {
+                            ParkId = model.Id,
+                            FacilityId = facilityId,
+                            Cnt = cnt,
+                            IsDelete = false,
+                            CreateTime = DateTime.Now,
+                            CreateBy = "user",
+                            UpdateTime = DateTime.Now,
+                            UpdateBy = "user"
+                        };
+                        db.ParkFacility.Add(fmodel);
+                    }
+                    #endregion
+
+                    db.SaveChanges();
                 }
-                db.SaveChanges();
+                scope.Complete();
             }
         }
     }
