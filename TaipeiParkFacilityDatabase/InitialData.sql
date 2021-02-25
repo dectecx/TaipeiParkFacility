052c82f78019193e5f5174d5dbd32e90398c6400
ts6075 <75653667+ts6075@users.noreply.github.com>
2021-02-25 13:28:37 +0800

    feat: add Initial Data SQL

---
+++ TaipeiParkFacilityDatabase/InitialData.sql
@@ -0,0 +1,49 @@
+﻿
+USE [TaipeiParkFacility]
+GO
+INSERT [dbo].[DimDict] ([Id], [Name], [IsDelete], [CreateTime], [CreateBy], [UpdateTime], [UpdateBy]) VALUES (1, N'中正區', 0, CAST(N'2021-02-25T04:54:02.313' AS DateTime), N'Initial ', CAST(N'2021-02-25T04:54:02.313' AS DateTime), N'Initial ')
+GO
+INSERT [dbo].[DimDict] ([Id], [Name], [IsDelete], [CreateTime], [CreateBy], [UpdateTime], [UpdateBy]) VALUES (2, N'大同區', 0, CAST(N'2021-02-25T04:54:02.453' AS DateTime), N'Initial ', CAST(N'2021-02-25T04:54:02.453' AS DateTime), N'Initial ')
+GO
+INSERT [dbo].[DimDict] ([Id], [Name], [IsDelete], [CreateTime], [CreateBy], [UpdateTime], [UpdateBy]) VALUES (3, N'中山區', 0, CAST(N'2021-02-25T04:54:02.453' AS DateTime), N'Initial ', CAST(N'2021-02-25T04:54:02.453' AS DateTime), N'Initial ')
+GO
+INSERT [dbo].[DimDict] ([Id], [Name], [IsDelete], [CreateTime], [CreateBy], [UpdateTime], [UpdateBy]) VALUES (4, N'松山區', 0, CAST(N'2021-02-25T04:54:02.453' AS DateTime), N'Initial ', CAST(N'2021-02-25T04:54:02.453' AS DateTime), N'Initial ')
+GO
+INSERT [dbo].[DimDict] ([Id], [Name], [IsDelete], [CreateTime], [CreateBy], [UpdateTime], [UpdateBy]) VALUES (5, N'大安區', 0, CAST(N'2021-02-25T04:54:02.453' AS DateTime), N'Initial ', CAST(N'2021-02-25T04:54:02.453' AS DateTime), N'Initial ')
+GO
+INSERT [dbo].[DimDict] ([Id], [Name], [IsDelete], [CreateTime], [CreateBy], [UpdateTime], [UpdateBy]) VALUES (6, N'萬華區', 0, CAST(N'2021-02-25T04:54:02.457' AS DateTime), N'Initial ', CAST(N'2021-02-25T04:54:02.457' AS DateTime), N'Initial ')
+GO
+INSERT [dbo].[DimDict] ([Id], [Name], [IsDelete], [CreateTime], [CreateBy], [UpdateTime], [UpdateBy]) VALUES (7, N'信義區', 0, CAST(N'2021-02-25T04:54:02.457' AS DateTime), N'Initial ', CAST(N'2021-02-25T04:54:02.457' AS DateTime), N'Initial ')
+GO
+INSERT [dbo].[DimDict] ([Id], [Name], [IsDelete], [CreateTime], [CreateBy], [UpdateTime], [UpdateBy]) VALUES (8, N'士林區', 0, CAST(N'2021-02-25T04:54:02.457' AS DateTime), N'Initial ', CAST(N'2021-02-25T04:54:02.457' AS DateTime), N'Initial ')
+GO
+INSERT [dbo].[DimDict] ([Id], [Name], [IsDelete], [CreateTime], [CreateBy], [UpdateTime], [UpdateBy]) VALUES (9, N'北投區', 0, CAST(N'2021-02-25T04:54:02.460' AS DateTime), N'Initial ', CAST(N'2021-02-25T04:54:02.460' AS DateTime), N'Initial ')
+GO
+INSERT [dbo].[DimDict] ([Id], [Name], [IsDelete], [CreateTime], [CreateBy], [UpdateTime], [UpdateBy]) VALUES (10, N'內湖區', 0, CAST(N'2021-02-25T04:54:02.460' AS DateTime), N'Initial ', CAST(N'2021-02-25T04:54:02.460' AS DateTime), N'Initial ')
+GO
+INSERT [dbo].[DimDict] ([Id], [Name], [IsDelete], [CreateTime], [CreateBy], [UpdateTime], [UpdateBy]) VALUES (11, N'南港區', 0, CAST(N'2021-02-25T04:54:02.460' AS DateTime), N'Initial ', CAST(N'2021-02-25T04:54:02.460' AS DateTime), N'Initial ')
+GO
+INSERT [dbo].[DimDict] ([Id], [Name], [IsDelete], [CreateTime], [CreateBy], [UpdateTime], [UpdateBy]) VALUES (12, N'文山區', 0, CAST(N'2021-02-25T04:54:02.460' AS DateTime), N'Initial ', CAST(N'2021-02-25T04:54:02.460' AS DateTime), N'Initial ')
+GO
+SET IDENTITY_INSERT [dbo].[DimFacility] ON 
+GO
+INSERT [dbo].[DimFacility] ([Id], [Name], [IsDelete], [CreateTime], [CreateBy], [UpdateTime], [UpdateBy]) VALUES (1, N'組合遊具', 0, CAST(N'2021-02-25T05:54:09.943' AS DateTime), N'Initial ', CAST(N'2021-02-25T05:54:09.943' AS DateTime), N'Initial ')
+GO
+INSERT [dbo].[DimFacility] ([Id], [Name], [IsDelete], [CreateTime], [CreateBy], [UpdateTime], [UpdateBy]) VALUES (2, N'磨石滑梯', 0, CAST(N'2021-02-25T05:54:10.077' AS DateTime), N'Initial ', CAST(N'2021-02-25T05:54:10.077' AS DateTime), N'Initial ')
+GO
+INSERT [dbo].[DimFacility] ([Id], [Name], [IsDelete], [CreateTime], [CreateBy], [UpdateTime], [UpdateBy]) VALUES (3, N'鞦韆', 0, CAST(N'2021-02-25T05:54:10.077' AS DateTime), N'Initial ', CAST(N'2021-02-25T05:54:10.077' AS DateTime), N'Initial ')
+GO
+INSERT [dbo].[DimFacility] ([Id], [Name], [IsDelete], [CreateTime], [CreateBy], [UpdateTime], [UpdateBy]) VALUES (4, N'翹翹板', 0, CAST(N'2021-02-25T05:54:10.080' AS DateTime), N'Initial ', CAST(N'2021-02-25T05:54:10.080' AS DateTime), N'Initial ')
+GO
+INSERT [dbo].[DimFacility] ([Id], [Name], [IsDelete], [CreateTime], [CreateBy], [UpdateTime], [UpdateBy]) VALUES (5, N'搖搖樂', 0, CAST(N'2021-02-25T05:54:10.080' AS DateTime), N'Initial ', CAST(N'2021-02-25T05:54:10.080' AS DateTime), N'Initial ')
+GO
+INSERT [dbo].[DimFacility] ([Id], [Name], [IsDelete], [CreateTime], [CreateBy], [UpdateTime], [UpdateBy]) VALUES (6, N'攀爬組', 0, CAST(N'2021-02-25T05:54:10.080' AS DateTime), N'Initial ', CAST(N'2021-02-25T05:54:10.080' AS DateTime), N'Initial ')
+GO
+INSERT [dbo].[DimFacility] ([Id], [Name], [IsDelete], [CreateTime], [CreateBy], [UpdateTime], [UpdateBy]) VALUES (7, N'戲沙坑', 0, CAST(N'2021-02-25T05:54:10.080' AS DateTime), N'Initial ', CAST(N'2021-02-25T05:54:10.080' AS DateTime), N'Initial ')
+GO
+INSERT [dbo].[DimFacility] ([Id], [Name], [IsDelete], [CreateTime], [CreateBy], [UpdateTime], [UpdateBy]) VALUES (8, N'擺盪船索', 0, CAST(N'2021-02-25T05:54:10.080' AS DateTime), N'Initial ', CAST(N'2021-02-25T05:54:10.080' AS DateTime), N'Initial ')
+GO
+INSERT [dbo].[DimFacility] ([Id], [Name], [IsDelete], [CreateTime], [CreateBy], [UpdateTime], [UpdateBy]) VALUES (9, N'其他', 0, CAST(N'2021-02-25T05:54:10.080' AS DateTime), N'Initial ', CAST(N'2021-02-25T05:54:10.080' AS DateTime), N'Initial ')
+GO
+SET IDENTITY_INSERT [dbo].[DimFacility] OFF
+GO
